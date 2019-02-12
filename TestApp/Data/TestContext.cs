4a9a4fbda1feb851dd88c4cd5dad03749f38ca3a
JoasE <32096708+JoasE@users.noreply.github.com>
2019-02-12 09:45:25 +0100

    Initial commit

---
+++ TestApp/Data/TestContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestApp.Models;
+
+namespace TestApp.Data
+{
+    public class TestContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
